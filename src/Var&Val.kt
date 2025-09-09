58a8634bc6eaf0e7992141812be1ea01f1f65962
TaufikAkmal12 <akmaltaufik08@gmail.com>
2025-09-09 22:03:03 +0700

    Commit Var&Val Kotlin

---
+++ src/Var&Val.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian Nilai Pertama Pada VarVariabel = " + varVariabel)
+    varVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian Nilai Kedua Pada VarVariabel = " + varVariabel)
+    var phi = 3.14
+    phi = 2.5
+}
