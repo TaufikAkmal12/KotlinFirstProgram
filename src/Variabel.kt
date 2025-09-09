c0b625e97431c2eb94d5aaad1a63f756144a558d
TaufikAkmal12 <akmaltaufik08@gmail.com>
2025-09-09 21:13:33 +0700

    Commit Variabel Kotlin

---
+++ src/Variabel.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamic Variabel = " + dynamicVar)
+
+    var staticVar: Char = 'A'
+    println("Contoh static Variabel = " + staticVar)
+}
